4c42914f840dfb17ce7b95035abc83a113c07c16
khanhtran0599 <khanhtran0599@gmail.com>
2021-05-28 15:28:31 +0700

    validate textfield

--- lib/screens/classroom/new_notify_class.dart
+++ lib/screens/classroom/new_notify_class.dart
@@ -18,6 +18,7 @@ class _NewNotifyState extends State<NewNotify> {
   bool expaned = false;
   PostDatabase postDatabase = PostDatabase();
   String title, content;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -276,128 +277,140 @@ class _NewNotifyState extends State<NewNotify> {
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      TextField(
-                        onChanged: (value) {},
-                        style:
-                            TextStyle(fontSize: 20, color: ColorApp.mediumBlue),
-                        decoration: InputDecoration(
-                            // border: InputBorder.none,
-                            isCollapsed: true,
-                            hintText: 'Câu hỏi 1',
-                            hintStyle:
-                                TextStyle(fontSize: 16, color: ColorApp.black)),
-                      ),
-                      TextField(
-                        onChanged: (value) {},
-                        style:
-                            TextStyle(fontSize: 20, color: ColorApp.mediumBlue),
-                        decoration: InputDecoration(
-                            // border: InputBorder.none,
-                            isCollapsed: true,
-                            hintText: 'A --> Đán áp đúng',
-                            hintStyle:
-                                TextStyle(fontSize: 16, color: ColorApp.black)),
-                      ),
-                      TextField(
-                        onChanged: (value) {},
-                        style:
-                            TextStyle(fontSize: 20, color: ColorApp.mediumBlue),
-                        decoration: InputDecoration(
-                            // border: InputBorder.none,
-                            isCollapsed: true,
-                            hintText: 'B',
-                            hintStyle:
-                                TextStyle(fontSize: 16, color: ColorApp.black)),
-                      ),
-                      TextField(
-                        onChanged: (value) {},
-                        style:
-                            TextStyle(fontSize: 20, color: ColorApp.mediumBlue),
-                        decoration: InputDecoration(
-                            // border: InputBorder.none,
-                            isCollapsed: true,
-                            hintText: 'C',
-                            hintStyle:
-                                TextStyle(fontSize: 16, color: ColorApp.black)),
-                      ),
-                      TextField(
-                        onChanged: (value) {},
-                        style:
-                            TextStyle(fontSize: 20, color: ColorApp.mediumBlue),
-                        decoration: InputDecoration(
-                            // border: InputBorder.none,
-                            isCollapsed: true,
-                            hintText: 'D',
-                            hintStyle:
-                                TextStyle(fontSize: 16, color: ColorApp.black)),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          ElevatedButton(
-                              child: Container(
-                                margin: EdgeInsets.symmetric(vertical: 10),
-                                child: Text("Hoàn thành",
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.normal)),
-                              ),
-                              style: ButtonStyle(
-                                  tapTargetSize: MaterialTapTargetSize.padded,
-                                  shadowColor: MaterialStateProperty.all<Color>(
-                                      Colors.lightBlue),
-                                  foregroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                          Colors.white),
-                                  backgroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                          ColorApp.mediumBlue),
-                                  shape: MaterialStateProperty.all<
-                                          RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          side: BorderSide(
-                                              color: Colors.transparent)))),
-                              onPressed: () async {
-                                final pdfFile =
-                                    await PdfParagraphApi.generate();
-                                PdfApi.openFile(pdfFile);
-                              }),
-                          ElevatedButton(
-                              child: Container(
-                                margin: EdgeInsets.symmetric(vertical: 10),
-                                child: Text("Tiếp tục",
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        // letterSpacing: 1,
-                                        // wordSpacing: 1,
-                                        fontWeight: FontWeight.normal)),
-                              ),
-                              style: ButtonStyle(
-                                  tapTargetSize: MaterialTapTargetSize.padded,
-                                  shadowColor: MaterialStateProperty.all<Color>(
-                                      Colors.lightBlue),
-                                  foregroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                          Colors.white),
-                                  backgroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                          ColorApp.mediumBlue),
-                                  shape: MaterialStateProperty.all<
-                                          RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          side: BorderSide(
-                                              color: Colors.transparent)))),
-                              onPressed: () async {}),
-                        ],
-                      )
-                    ],
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        TextFormField(
+                          validator: (val) =>
+                              val.isEmpty ? 'Nhập câu hỏi' : null,
+                          onChanged: (value) {},
+                          style: TextStyle(
+                              fontSize: 20, color: ColorApp.mediumBlue),
+                          decoration: InputDecoration(
+                              // border: InputBorder.none,
+                              isCollapsed: true,
+                              hintText: 'Câu hỏi 1',
+                              hintStyle: TextStyle(
+                                  fontSize: 16, color: ColorApp.black)),
+                        ),
+                        TextFormField(
+                          validator: (val) =>
+                              val.isEmpty ? 'Nhập câu trả lời đúng' : null,
+                          onChanged: (value) {},
+                          style: TextStyle(
+                              fontSize: 20, color: ColorApp.mediumBlue),
+                          decoration: InputDecoration(
+                              // border: InputBorder.none,
+                              isCollapsed: true,
+                              hintText: 'A --> Đán áp đúng',
+                              hintStyle: TextStyle(
+                                  fontSize: 16, color: ColorApp.black)),
+                        ),
+                        TextFormField(
+                          validator: (val) => val.isEmpty
+                              ? 'Nhập ít nhất 1 câu trả lời sai'
+                              : null,
+                          onChanged: (value) {},
+                          style: TextStyle(
+                              fontSize: 20, color: ColorApp.mediumBlue),
+                          decoration: InputDecoration(
+                              // border: InputBorder.none,
+                              isCollapsed: true,
+                              hintText: 'B',
+                              hintStyle: TextStyle(
+                                  fontSize: 16, color: ColorApp.black)),
+                        ),
+                        TextFormField(
+                          onChanged: (value) {},
+                          style: TextStyle(
+                              fontSize: 20, color: ColorApp.mediumBlue),
+                          decoration: InputDecoration(
+                              // border: InputBorder.none,
+                              isCollapsed: true,
+                              hintText: 'C',
+                              hintStyle: TextStyle(
+                                  fontSize: 16, color: ColorApp.black)),
+                        ),
+                        TextFormField(
+                          onChanged: (value) {},
+                          style: TextStyle(
+                              fontSize: 20, color: ColorApp.mediumBlue),
+                          decoration: InputDecoration(
+                              // border: InputBorder.none,
+                              isCollapsed: true,
+                              hintText: 'D',
+                              hintStyle: TextStyle(
+                                  fontSize: 16, color: ColorApp.black)),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            ElevatedButton(
+                                child: Container(
+                                  margin: EdgeInsets.symmetric(vertical: 10),
+                                  child: Text("Hoàn thành",
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.normal)),
+                                ),
+                                style: ButtonStyle(
+                                    tapTargetSize: MaterialTapTargetSize.padded,
+                                    shadowColor: MaterialStateProperty.all<Color>(
+                                        Colors.lightBlue),
+                                    foregroundColor: MaterialStateProperty.all<Color>(
+                                        Colors.white),
+                                    backgroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            ColorApp.mediumBlue),
+                                    shape: MaterialStateProperty.all<
+                                            RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            side: BorderSide(
+                                                color: Colors.transparent)))),
+                                onPressed: () async {
+                                  final pdfFile =
+                                      await PdfParagraphApi.generate();
+                                  PdfApi.openFile(pdfFile);
+                                }),
+                            ElevatedButton(
+                                child: Container(
+                                  margin: EdgeInsets.symmetric(vertical: 10),
+                                  child: Text("Tiếp tục",
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          // letterSpacing: 1,
+                                          // wordSpacing: 1,
+                                          fontWeight: FontWeight.normal)),
+                                ),
+                                style: ButtonStyle(
+                                    tapTargetSize: MaterialTapTargetSize.padded,
+                                    shadowColor: MaterialStateProperty.all<Color>(
+                                        Colors.lightBlue),
+                                    foregroundColor: MaterialStateProperty.all<Color>(
+                                        Colors.white),
+                                    backgroundColor:
+                                        MaterialStateProperty.all<Color>(
+                                            ColorApp.mediumBlue),
+                                    shape: MaterialStateProperty.all<
+                                            RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            side: BorderSide(
+                                                color: Colors.transparent)))),
+                                onPressed: () async {
+                                  if (_formKey.currentState.validate()) {
+                                    print('validated');
+                                  }
+                                }),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 secondChild: Container(),
